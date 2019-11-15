2018ff12ae8068ecea68ea3648fbd763f5d7a0dd
nyutoplus <tttmasapi@gmail.com>
2019-11-16 03:41:54 +0900

    コミット

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import assets.util.Config;
 import assets.util.Logger;
 import assets.util.Register;
 
@@ -7,13 +8,18 @@ public class Main {
 	@SuppressWarnings("unused")
 	private static final long serialVersionUID = 1L;
 
-	public static Logger logger = new Logger(System.out);
+	public static Logger logger;
 
-	public static Register register = new Register();
+	public static Register register;
 
 	public static void main(String[] args) {
+		logger = new Logger(System.out);
 		System.setOut(logger);
-
+		register = new Register();
+//		System.out.println(System.);
+		Config config = new Config();
+		config.readConfig("/defaultconfig.conf");
+//		config.con("/defaultconfig.conf");
 		register.register("test","test");
 		//下のコメントアウトした部分を有効にするとキーが重複しているので例外が発生します
 		//register.register("test","test2");
