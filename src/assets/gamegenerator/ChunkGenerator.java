24471d823c8a35870c38791a9ac673d63fbeecb1
nyutoplus <tttmasapi@gmail.com>
2020-02-14 05:24:27 +0900

    create chunk generator template

---
+++ src/assets/gamegenerator/ChunkGenerator.java
@@ -0,0 +1,60 @@
+/**
+ * チャンク生成器のテンプレートです
+ *
+ * @author nyuto
+ * @version 1.0
+ */
+
+package assets.gamegenerator;
+
+import assets.taskutil.Block;
+import assets.taskutil.Chunk;
+import assets.taskutil.Ground;
+import assets.taskutil.Ore;
+
+public abstract class ChunkGenerator {
+	@SuppressWarnings("unused")
+	private static final long serialVersionUID = 1L;
+
+
+	public abstract void setSeed(long seed);
+
+	public abstract Chunk getChunk(int x,int y);
+
+	public abstract void addUseGround(Ground... grounds);
+
+	public abstract void addUseGround(Ground ground,double frequency);
+
+	public abstract void addUseGround(Ground[] grounds,double frequency);
+
+	public abstract void addUseGround(Ground[] grounds,double[] frequency);
+
+	public abstract void addUseBlock(Block... block);
+
+	public abstract void addUseBlock(Block block,double frequency);
+
+	public abstract void addUseBlock(Block[] blocks,double frequency);
+
+	public abstract void addUseBlock(Block[] blocks,double[] frequency);
+
+	public abstract void addUseOre(Ore... ore);
+
+	public abstract void addUseOre(Ore ore,double frequency);
+
+	public abstract void addUseOre(Ore[] ores,double frequency);
+
+	public abstract void addUseOre(Ore[] ores,double[] frequency);
+
+
+	public abstract Ground[] getUseGroundArray();
+
+	public abstract double[] getGroundFrequencyArray();
+
+	public abstract Block[] getUseBlockArray();
+
+	public abstract double[] getBlockFrequencyArray();
+
+	public abstract Ore[] getUseOreArray();
+
+	public abstract double[] getOreFrequencyArray();
+}
