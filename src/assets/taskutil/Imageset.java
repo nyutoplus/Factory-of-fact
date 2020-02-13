69748d5780a9016d2c0f32b46f729df4d854eaa1
nyutoplus <tttmasapi@gmail.com>
2020-02-14 04:09:25 +0900

    add serialVersionUID

--- src/assets/taskutil/Imageset.java
+++ src/assets/taskutil/Imageset.java
@@ -1,8 +1,17 @@
+/**
+ * 描画用イメージ保持用クラスです
+ *
+ * @author nyuto
+ * @version 1.0
+ */
+
 package assets.taskutil;
 
 import java.awt.Image;
 
 public class Imageset {
+	@SuppressWarnings("unused")
+	private static final long serialVersionUID = 1L;
 
 	private Image[] images;
 
